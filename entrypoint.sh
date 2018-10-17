39790cfa432be40e405310eb0bc3d2932ae13633
Benjamin Pannell <admin@sierrasoftworks.com>
2018-10-17 15:50:22 +0100

    fix: Provide a way to initialize the pod

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,8 @@ set -e -o pipefail
 mc config host add acme "$MINIO_SERVER" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY" "$MINIO_API_VERSION" > /dev/null
 
 mc cp --recursive acme/${MINIO_BUCKET}/ ${SECRETS_VOLUME}/
-mc mirror -w --overwrite $SECRETS_VOLUME/ acme/${MINIO_BUCKET}
+chmod 600 ${SECRETS_VOLUME}/*
+
+if [[ "${1-watch}" == "watch" ]]; then
+    mc mirror -w --overwrite $SECRETS_VOLUME/ acme/${MINIO_BUCKET}
+fi
